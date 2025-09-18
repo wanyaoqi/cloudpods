aa3229518f8a6ebf146d5a8de3f5b689ac3e2708
wanyaoqi <wanyaoqi@yunion.cn>
2025-09-18 08:33:42 +0800

    temp

--- pkg/compute/models/guestscreendump.go
+++ pkg/compute/models/guestscreendump.go
@@ -9,6 +9,7 @@ import (
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/pkg/errors"
 	"yunion.io/x/sqlchemy"
+	"yunion.io/x/log"
 
 	api "yunion.io/x/onecloud/pkg/apis/compute"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
