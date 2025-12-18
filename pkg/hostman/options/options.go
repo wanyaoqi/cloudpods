6053b0af351855a88fb5fd2968c3207375c81de6
wanyaoqi <d3lx.yq@gmail.com>
2025-12-18 10:14:12 +0800

    temp

--- pkg/hostman/options/options.go
+++ pkg/hostman/options/options.go
@@ -106,7 +106,7 @@ type SHostOptions struct {
 
 	ChntpwPath          string `help:"path to chntpw tool" default:"/usr/local/bin/chntpw.static"`
 	OvmfPath            string `help:"Path to OVMF.fd" default:"/opt/cloud/contrib/OVMF.fd"`
-	SecbootOvmfPath     string `help:"Path to secboot ovmf fd" default:"/opt/cloud/contrib/OVMF_CODE_4M.fd"`
+	SecbootOvmfPath     string `help:"Path to secboot ovmf fd" default:"/opt/cloud/contrib/OVMF_CODE_4M.secboot.fd"`
 	SecbootOvmfVarsPath string `help:"Path to secboot ovmf vars fd" default:"/opt/cloud/contrib/OVMF_VARS_4M.fd"`
 
 	LinuxDefaultRootUser    bool `help:"Default account for linux system is root"`
