772f8fc8f0f2271d596df682b3bdc3afc6056067
wanyaoqi <wanyaoqi@yunion.cn>
2026-04-16 20:03:30 +0800

    fix: services addapp-options handle

--- pkg/ansibleserver/service/handlers.go
+++ pkg/ansibleserver/service/handlers.go
@@ -32,11 +32,15 @@ import (
 	"yunion.io/x/onecloud/pkg/ansibleserver/models"
 	"yunion.io/x/onecloud/pkg/appsrv"
 	"yunion.io/x/onecloud/pkg/appsrv/dispatcher"
+	app_common "yunion.io/x/onecloud/pkg/cloudcommon/app"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
+	"yunion.io/x/onecloud/pkg/ansibleserver/options"
 )
 
 func InitHandlers(app *appsrv.Application, isSlave bool) {
 	db.InitAllManagers()
+
+	app_common.ExportOptionsHandler(app, &options.Options)
 
 	db.RegisterModelManager(db.OpsLog)
 	db.RegisterModelManager(db.Metadata)
